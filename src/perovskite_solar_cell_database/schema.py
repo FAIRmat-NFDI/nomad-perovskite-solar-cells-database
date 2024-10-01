6566877c0ae32c17b0be237d63bb3f3ba7ee2152
Sherjeel Shabih <shabihsherjeel@gmail.com>
2024-10-01 14:57:59 +0200

    Adds a draft of the LLM extracted section

--- src/perovskite_solar_cell_database/schema.py
+++ src/perovskite_solar_cell_database/schema.py
@@ -5,8 +5,9 @@ from typing import (
 if TYPE_CHECKING:
     pass
 
-from nomad.datamodel.data import Schema, UseCaseElnCategory
-from nomad.metainfo import SchemaPackage, Section, SubSection
+from nomad.datamodel.data import Schema, UseCaseElnCategory, ArchiveSection
+from nomad.metainfo import SchemaPackage, Section, SubSection, Quantity, Datetime
+import numpy as np
 
 from .schema_sections import (
     EQE,
@@ -59,6 +60,86 @@ class PerovskiteSolarCell(Schema):
     eqe = SubSection(section_def=EQE)
     stability = SubSection(section_def=Stability)
     outdoor = SubSection(section_def=Outdoor)
+
+
+class PipelineMetrics(ArchiveSection):
+    precision = Quantity(
+        type=np.float32,
+        description='The precision of the results calculated during pipeline design.',
+    )
+
+    recall = Quantity(
+        type=np.float32,
+        description='The recall of the results calculated during pipeline design.',
+    )
+
+
+class ModelSettings(ArchiveSection):
+    temperature = Quantity(
+        type=np.float32, description='The temperature setting of the LLM model.'
+    )
+
+    top_p = Quantity(type=np.float32, description='The Top P setting of the LLM model.')
+
+    max_tokens = Quantity(
+        type=int, description='The max tokens setting of the LLM model.'
+    )
+
+
+class LLMPipeline(ArchiveSection):
+    pipeline_name = Quantity(type=str, description='The name of the LLM pipeline.')
+
+    prompt_template = Quantity(
+        type=str,
+        description="""The prompt template used by these pipeline. Variables are
+                        listed between curly brackets, {{}}. These variables should
+                        be listed within the corresponding LLMActivity instance as
+                        Quantities of type str.
+
+                        For example:
+                        How many r's are there in {{context}}?
+                        """,
+    )
+
+    # steps = SubSection( # Just for visualization.
+    #     section_def=Workflow,
+    # )
+
+    model_name = Quantity(
+        type=str,
+        description='The name and version, if possible, of the model this pipeline was calibrated against.',
+    )
+
+    settings = SubSection(
+        section_def=ModelSettings,
+    )
+
+    metrics = SubSection(
+        section_def=PipelineMetrics,
+    )
+
+    repository = Quantity(
+        type=str,  # this can be a URL or Link check it out.
+        description='The repository link pointing to a specific commit, if applicable, where the code for the pipeline exists.',
+    )
+
+
+class LLMExtractedPerovskiteSolarCell(PerovskiteSolarCell):
+    # llm_extraction_pipeline = SubSection(section_def=BreathingPerovskiteDBPipeline)
+    llm_extraction_pipeline = SubSection(section_def=LLMPipeline)
+
+    timestamp = Quantity(
+        type=Datetime,
+        a_eln=dict(component='DateTimeEditQuantity'),
+        description='The time when this extraction was made.',
+    )
+
+    doi = Quantity(
+        type=str,
+        shape=[],
+        description='The DOI of the resource the solar cell was extracted from.',
+        a_eln=dict(component='EnumEditQuantity', props=dict(suggestions=[])),
+    )
 
 
 m_package.__init_metainfo__()
