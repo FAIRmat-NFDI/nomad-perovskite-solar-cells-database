1fcbddf55a9d7126f14d37027f8e6ab167b53863
Fabian Schöppach <fabian.schoeppach@physik.hu-berlin.de>
2024-10-07 16:29:53 +0200

    First draft of a TandemCell class

--- src/perovskite_solar_cell_database/schema.py
+++ src/perovskite_solar_cell_database/schema.py
@@ -115,5 +115,44 @@ class PerovskiteSolarCell(Schema, PlotSection):
 
         self.figures = [PlotlyFigure(figure=fig.to_plotly_json())]
 
+class PerovskiteTandemSolarCell(Schema):
+    """
+    This schema is adapted to map the data in the [Perovskite Solar Cell Database
+    Project](https://www.perovskitedatabase.com/). The descriptions in the quantities
+    represent the instructions given to the user who manually curated the data.
+    """
+
+    m_def = Section(
+        label='Perovskite Tandem Solar Cell',
+        a_eln=dict(lane_width='400px'),
+        categories=[UseCaseElnCategory],
+    )
+
+    # Reference
+    ref = SubSection(section_def=Ref)
+
+    # Cell Stack as from tandem instructions v4.0
+    cell = SubSection(section_def=Cell)
+    module = SubSection(section_def=Module)
+    substrate = SubSection(section_def=Substrate)
+    backcontact = SubSection(section_def=Backcontact)
+    absorber_bottom = SubSection(section_def=Perovskite) # TODO: Change to CIGS related section
+    buffer = SubSection(section_def=ETL) # TODO: Change to buffer related section
+    frontcontact_bottom = SubSection(section_def=Backcontact) # TODO: Change to front contact related section
+    htl = SubSection(section_def=HTL)
+    absorber_top = SubSection(section_def=Perovskite)
+    absorber_top_deposition = SubSection(section_def=PerovskiteDeposition)
+    etl = SubSection(section_def=ETL)
+    frontcontact = SubSection(section_def=Backcontact) # TODO: Change to front contact related section
+    add = SubSection(section_def=Add)
+
+    # Miscellaneous
+    encapsulation = SubSection(section_def=Encapsulation)
+    jv = SubSection(section_def=JV)
+    stabilised = SubSection(section_def=Stabilised)
+    eqe = SubSection(section_def=EQE)
+    stability = SubSection(section_def=Stability)
+    outdoor = SubSection(section_def=Outdoor)
+
 
 m_package.__init_metainfo__()
