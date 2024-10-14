df432e0cf4c71cfceac2ea1527da9867448056ad
Fabian Schöppach <fabian.schoeppach@physik.hu-berlin.de>
2024-10-14 10:55:03 +0200

    Refactor tandem database code into separate package

--- src/perovskite_tandem_database/schema.py
+++ src/perovskite_tandem_database/schema.py
@@ -8,7 +8,7 @@ if TYPE_CHECKING:
 from nomad.datamodel.data import Schema, UseCaseElnCategory
 from nomad.metainfo import SchemaPackage, Section, SubSection
 
-from .schema_sections import (
+from perovskite_solar_cell_database.schema_sections import (
     EQE,
     ETL,
     HTL,
@@ -30,7 +30,7 @@ from .schema_sections import (
 m_package = SchemaPackage()
 
 
-class PerovskiteSolarCell(Schema):
+class PerovskiteTandemSolarCell(Schema):
     """
     This schema is adapted to map the data in the [Perovskite Solar Cell Database
     Project](https://www.perovskitedatabase.com/). The descriptions in the quantities
@@ -38,26 +38,37 @@ class PerovskiteSolarCell(Schema):
     """
 
     m_def = Section(
-        label='Perovskite Solar Cell',
+        label='Perovskite Tandem Solar Cell',
         a_eln=dict(lane_width='400px'),
         categories=[UseCaseElnCategory],
     )
 
+    # Reference
     ref = SubSection(section_def=Ref)
+
+    # Cell Stack as from tandem instructions v4.0
     cell = SubSection(section_def=Cell)
     module = SubSection(section_def=Module)
     substrate = SubSection(section_def=Substrate)
-    etl = SubSection(section_def=ETL)
-    perovskite = SubSection(section_def=Perovskite)
-    perovskite_deposition = SubSection(section_def=PerovskiteDeposition)
-    htl = SubSection(section_def=HTL)
     backcontact = SubSection(section_def=Backcontact)
+    absorber_bottom = SubSection(section_def=Perovskite) # TODO: Change to CIGS related section
+    absorber_bottom_deposition = SubSection(section_def=PerovskiteDeposition) # TODO: Change to CIGS related section
+    buffer = SubSection(section_def=ETL) # TODO: Change to buffer related section
+    frontcontact_bottom = SubSection(section_def=Backcontact) # TODO: Change to front contact related section
+    htl = SubSection(section_def=HTL)
+    absorber_top = SubSection(section_def=Perovskite)
+    absorber_top_deposition = SubSection(section_def=PerovskiteDeposition)
+    etl = SubSection(section_def=ETL)
+    frontcontact = SubSection(section_def=Backcontact) # TODO: Change to front contact related section
     add = SubSection(section_def=Add)
+
+    # Miscellaneous
     encapsulation = SubSection(section_def=Encapsulation)
     jv = SubSection(section_def=JV)
     stabilised = SubSection(section_def=Stabilised)
     eqe = SubSection(section_def=EQE)
     stability = SubSection(section_def=Stability)
     outdoor = SubSection(section_def=Outdoor)
+
 
 m_package.__init_metainfo__()
