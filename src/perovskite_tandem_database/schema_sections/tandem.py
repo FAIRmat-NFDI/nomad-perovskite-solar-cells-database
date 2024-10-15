06607ea4e318f6881272f5960e9680368e4ab617
Fabian Schöppach <fabian.schoeppach@physik.hu-berlin.de>
2024-10-15 17:58:23 +0200

    Refactor tandem.py to complete layer properties

--- src/perovskite_tandem_database/schema_sections/tandem.py
+++ src/perovskite_tandem_database/schema_sections/tandem.py
@@ -7,6 +7,8 @@ from nomad.datamodel.metainfo.basesections import (
 from nomad.metainfo import Quantity, SubSection
 from nomad.metainfo.data_type import Enum
 
+from perovskite_solar_cell_database.schema_sections.ions.ion import Ion
+
 # Chemicals and materials and their treatment
 
 class SolventAnnealing(ArchiveSection):
@@ -88,19 +90,27 @@ class Layer(ArchiveSection):
     surface_roughness = Quantity()
 
     # Origin and manufacturing
-    origin = Quantity()
+    origin = Quantity(
+        type = Enum(['Commercial', 'Lab made', 'Unknown']),
+        description='Origin of the layer',
+        shape=[],
+    )
     supplier = Quantity()
     supplier_brand = Quantity()
     synthesis = SubSection(Synthesis)
 
     # Storage
     storage = SubSection(Storage)
+
+    # Common properties
+    additives = SubSection(ElementalComposition, repeating=True)
   
 
 class NonAbsorbingLayer(Layer):
-    additivies = Quantity(
-        section_def=ElementalComposition,
-    )
+    pass
+
+class Substrate(NonAbsorbingLayer):
+    pass
 
 class PhotoAbsorber(Layer):
     bandgap = Quantity()
@@ -108,14 +118,44 @@ class PhotoAbsorber(Layer):
     bandgap_estimation_basis = Quantity()
     PL_max = Quantity()
 
+    # Composition
+    additives = SubSection(ElementalComposition, repeating=True)
+
+    # Misc
+    perovskite_inspired = Quantity()
+
+class PerovskiteComposition(ArchiveSection):
+    basis = Quantity()
+    ion_a = SubSection(Ion, repeating=True)
+    ion_b = SubSection(Ion, repeating=True)
+    ion_c = SubSection(Ion, repeating=True)
+
+
 class PerovskiteLayer(PhotoAbsorber):
-    pass
+    dimension = Quantity(
+        type=Enum(['0D (Quantum dot)', '2D', '2D/3D mixture', '3D', '3D with 2D capping layer']),
+        description='''
+            The dimension of the perovskite layer.''',
+        shape=[],
+        a_eln=dict(component='EnumEditQuantity'))
+    composition = SubSection(PerovskiteComposition)
+
+    # General properties
+    single_crystal = Quantity()
+    stoichiometry = Quantity()
+    inorganic = Quantity()
+    lead_free = Quantity()
 
 class SiliconLayer(PhotoAbsorber):
-    pass
+    cell_type = Quantity()
+    silicon_type = Quantity()
+    doping_sequence = Quantity()
+    
 
 class ChalcopyriteLayer(PhotoAbsorber):
-    pass
+    composition = SubSection(ElementalComposition, repeating=True)
+    alkali_metal_doping = Quantity()
+    alkali_metal_doping_sources = Quantity()
 
 # Device architecture
 
@@ -124,54 +164,59 @@ class Tandem(EntryData):
     This section stores general configuration information about a tandem solar cell.
     """
 
-    architecture = Quantity(
-        type=str,
-        description='The general architecture of the tandem device. For 4-terminal devices and other configurations where there are two independent sub cells simply stacked on top of each other, define this as “stacked”',
-        shape=[],
-        a_eln=dict(
-            component='EnumEditQuantity',
-            props=dict(
-                suggestions=sorted(['stacked', 'monolithic', 'other'])
-            )
-        )
-    )
+    layer_stack = SubSection(
+        section_def = Layer,
+        description='The stack of layers in the device starting from the bottom.',
+        repeating=True)
 
-    number_of_terminals = Quantity(
-        type=int,
-        description='The number of terminals in the device. The most common configurations are 2 and 4',
-        shape=[],
-        a_eln=dict(component='NumberEditQuantity')
-    )
+    # architecture = Quantity(
+    #     type=str,
+    #     description='The general architecture of the tandem device. For 4-terminal devices and other configurations where there are two independent sub cells simply stacked on top of each other, define this as “stacked”',
+    #     shape=[],
+    #     a_eln=dict(
+    #         component='EnumEditQuantity',
+    #         props=dict(
+    #             suggestions=sorted(['stacked', 'monolithic', 'other'])
+    #         )
+    #     )
+    # )
 
-    number_of_junctions = Quantity(
-        type=int,
-        description='The number of junctions in the device.',
-        shape=[],
-        a_eln=dict(component='NumberEditQuantity')
-    )
+    # number_of_terminals = Quantity(
+    #     type=int,
+    #     description='The number of terminals in the device. The most common configurations are 2 and 4',
+    #     shape=[],
+    #     a_eln=dict(component='NumberEditQuantity')
+    # )
 
-    photoabsorber = Quantity(
-        type=Enum(['Silicon', 'Perovskite', 'CIGS', 'OPV', 'OSC', 'DSSC', 'BHJ']),
-        description='''
-            List of the photoabsorbers starting from the bottom of the device stack
-            - Start with the bottom one, i.e. the one with the lowest band gap that is closest to the substrate and to the left in the defined device stack
-            - Do not care about composition, e.g. write Perovskite or CIGS regardless of the composition of the perovskite and the CIGS. There are several fields that specifically deals with the composition.
-        ''',
-        shape=['*'],
-        a_eln=dict(component='EnumEditQuantity')
-    )
+    # number_of_junctions = Quantity(
+    #     type=int,
+    #     description='The number of junctions in the device.',
+    #     shape=[],
+    #     a_eln=dict(component='NumberEditQuantity')
+    # )
+
+    # photoabsorber = Quantity(
+    #     type=Enum(['Silicon', 'Perovskite', 'CIGS', 'OPV', 'OSC', 'DSSC', 'BHJ']),
+    #     description='''
+    #         List of the photoabsorbers starting from the bottom of the device stack
+    #         - Start with the bottom one, i.e. the one with the lowest band gap that is closest to the substrate and to the left in the defined device stack
+    #         - Do not care about composition, e.g. write Perovskite or CIGS regardless of the composition of the perovskite and the CIGS. There are several fields that specifically deals with the composition.
+    #     ''',
+    #     shape=['*'],
+    #     a_eln=dict(component='EnumEditQuantity')
+    # )
     
-    photoabsorber_bandgaps = Quantity(
-        type= float,
-        #unit = 'eV',
-        description='''
-            List of the band gap values of the respective photo absorbers
-            - Every layer should be separated by a space, a vertical bar, and a space, i.e. (‘ | ‘)
-            - The layers must line up with the previous filed.
-            - State band gaps in eV
-            - If there are uncertainties, state the best estimate, e.g write 1.5 and not 1.45-1.55
-            - Every photo absorber has a band gap. If it is unknown, state this as ‘nan’
-        ''',
-        shape=['*'],
-        a_eln=dict(component='NumberEditQuantity')
-    )
+    # photoabsorber_bandgaps = Quantity(
+    #     type= float,
+    #     #unit = 'eV',
+    #     description='''
+    #         List of the band gap values of the respective photo absorbers
+    #         - Every layer should be separated by a space, a vertical bar, and a space, i.e. (‘ | ‘)
+    #         - The layers must line up with the previous filed.
+    #         - State band gaps in eV
+    #         - If there are uncertainties, state the best estimate, e.g write 1.5 and not 1.45-1.55
+    #         - Every photo absorber has a band gap. If it is unknown, state this as ‘nan’
+    #     ''',
+    #     shape=['*'],
+    #     a_eln=dict(component='NumberEditQuantity')
+    # )
