db8b625fe1b8127e02efd4abc05577bd2dafbcf9
Fabian Schöppach <fabian.schoeppach@physik.hu-berlin.de>
2024-10-22 15:49:35 +0200

    Add basis and entry point for parser

---
+++ src/perovskite_tandem_database/parsers/__init__.py
@@ -0,0 +1,21 @@
+from nomad.config.models.plugins import ParserEntryPoint
+
+
+class TandemParserEntryPoint(ParserEntryPoint):
+    """
+    XRF Parser plugin entry point.
+    """
+
+    def load(self):
+        # lazy import to avoid circular dependencies
+        from perovskite_tandem_database.parsers.tandemparser import TandemParser
+
+        return TandemParser(**self.dict())
+
+
+tandemparser = TandemParserEntryPoint(
+    name='TandemParser',
+    description='Tandem Parser for .csv files.',
+    mainfile_name_re='.*\.csv',
+    #mainfile_content_re='',
+)
