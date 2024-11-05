65eeb6707adb26047a204eae5ef0bf55bc9b5fc5
Pepe Marquez <pepemarquezprieto@gmail.com>
2024-11-05 18:55:51 +0100

    First version of edit extraction schema

--- src/perovskite_solar_cell_database/__init__.py
+++ src/perovskite_solar_cell_database/__init__.py
@@ -13,7 +13,21 @@ class PerovskiteDatabasePackageEntryPoint(SchemaPackageEntryPoint):
         return m_package
 
 
+class LLMSchemaExtractionPackageEntryPoint(SchemaPackageEntryPoint):
+    def load(self):
+        from perovskite_solar_cell_database.llm_extraction_schema import (
+            m_package,
+        )
+
+        return m_package
+
+
 perovskite_solar_cell = PerovskiteDatabasePackageEntryPoint(
     name='PerovskiteSolarCell',
     description='Schema package defined for the perovskite solar cells database.',
+)
+
+llm_extraction_schema = LLMSchemaExtractionPackageEntryPoint(
+    name='LLMExtractionSchema',
+    description='Schema package defined for the perovskite solar cells database LLM extraction.',
 )
