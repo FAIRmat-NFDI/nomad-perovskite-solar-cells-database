dc1468a9857f32a26cf8b17c0685e4c09d39af23
Hampus Näsström <hampus.nasstrom@gmail.com>
2024-11-07 09:30:13 +0100

    Added ion parser

--- src/perovskite_solar_cell_database/__init__.py
+++ src/perovskite_solar_cell_database/__init__.py
@@ -1,4 +1,7 @@
-from nomad.config.models.plugins import SchemaPackageEntryPoint
+from nomad.config.models.plugins import (
+    ParserEntryPoint,
+    SchemaPackageEntryPoint,
+)
 from pydantic import Field
 
 
@@ -31,4 +34,22 @@ class PerovskiteCompositionEntryPoint(SchemaPackageEntryPoint):
 perovskite_composition = PerovskiteCompositionEntryPoint(
     name='PerovskiteComposition',
     description='Schema package defined for the perovskite compositions.',
+)
+
+
+class IonParserEntryPoint(ParserEntryPoint):
+    def load(self):
+        from perovskite_solar_cell_database.parser import IonParser
+
+        return IonParser(**self.dict())
+
+
+ion_parser = IonParserEntryPoint(
+    name='PerovskiteIonParser',
+    description='Parse excel files containing perovskite ions.',
+    mainfile_name_re=r'.+\.xlsx',
+    mainfile_mime_re='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    mainfile_contents_dict={
+        'Sheet1': {'__has_all_keys': ['perovskite_site', 'abbreviation', 'molecular_formula', 'smiles', 'common_name', 'iupac_name', 'cas_number', 'source_compound_iupac_name', 'source_compound_smiles', 'source_compound_cas_number']},
+    },
 )
---
+++ src/perovskite_solar_cell_database/parser.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+from nomad.datamodel import EntryArchive
+from nomad.parsing import MatchingParser
+
+from perovskite_solar_cell_database.composition import (
+    PerovskiteAIon,
+    PerovskiteBIon,
+    PerovskiteCIon,
+)
+from perovskite_solar_cell_database.utils import create_archive
+
+
+class IonParser(MatchingParser):
+    def parse(
+        self,
+        mainfile: str,
+        archive: EntryArchive,
+        logger=None,
+        child_archives: dict[str, EntryArchive] = None,
+    ) -> None:
+        df = pd.read_excel(mainfile).replace(np.nan, None)
+        for index, row in df.iterrows():
+            if row['perovskite_site'] == 'A':
+                ion = PerovskiteAIon()
+            elif row['perovskite_site'] == 'B':
+                ion = PerovskiteBIon()
+            elif row['perovskite_site'] == 'C':
+                ion = PerovskiteCIon()
+            else:
+                raise ValueError(f'Unknown ion type {row["perovskite_site"]}')
+            ion.abbreviation = row['abbreviation']
+            ion.molecular_formula = row['molecular_formula']
+            ion.smiles = row['smiles']
+            ion.common_name = row['common_name']
+            ion.iupac_name = row['iupac_name']
+            ion.cas_number = row['cas_number']
+            ion.source_compound_iupac_name = row['source_compound_iupac_name']
+            ion.source_compound_smiles = row['source_compound_smiles']
+            ion.source_compound_cas_number = row['source_compound_cas_number']
+            create_archive(ion, archive, f'{row['abbreviation']}_perovskite_ion.archive.json')
